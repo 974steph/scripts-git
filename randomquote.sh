1e554baf6dbd35181d3715b100eda3079520d3a4
Matt <matt.palermo@gmail.com>
2016-12-11 16:01:01 -0500

    initial commit

---
+++ randomquote.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+BASE_URL="http://www.lolsotrue.com"
+
+MAX_PAGE=$(curl -sL ${BASE_URL} | grep page= | sed 's/href/\n/g' | tail -n 2 | head -n1 | sed 's/.*page=\([0-9]\+\).*/\1/')
+
+
+PAGE=$(( ( RANDOM % ${MAX_PAGE} )  + 1 ))
+
+#echo "PAGE: $PAGE"
+
+QUOTE_URL="${BASE_URL}/?page=${PAGE}"
+
+QUOTE=$(curl -sL "${QUOTE_URL}" | awk '/thumbnails/,/javascript/' | grep image.*lolsotrue | sed 's/.*alt=\"\(.*\)\"\/>.*/\1/')
+
+#echo "QUOTE: ${QUOTE}"
+echo "${QUOTE}"
