db1e90e6b1c6f86471b8513e0d199c606a770815
Matt <matt.palermo@gmail.com>
2016-12-27 08:30:25 -0500

    updated

--- Dawnbringer.sh
+++ Dawnbringer.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl -s http://us.battle.net/wow/en/status | grep -i -B6 Dawnbringer | grep data-raw | awk '{print $3}' | sed -s "s/.*\"\(.*\)\".*/\1/"
+#curl -s http://us.battle.net/wow/en/status | grep -i -B6 Dawnbringer | grep data-raw | awk '{print $3}' | sed -s "s/.*\"\(.*\)\".*/\1/"
+
+curl -sL http://us.battle.net/wow/en/status | grep Dawnbringer | sed "s/.*SortTable-col.*Dawnbringer/Dawnbringer /g;s/United States.*/USA/;s/<[^>]\+>/ /g"
