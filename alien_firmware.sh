f164f2ceda4ae4d18d30f17f3ab61d40087d8f73
Matt <matt.palermo@gmail.com>
2017-02-01 18:01:55 -0500

    initial commitof alien firmware checker

---
+++ alien_firmware.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# DEBUG="YES"
+
+TMP_FILE="/tmp/alien_firmware_$(date +%s).tmp"
+
+curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null  > ${TMP_FILE}
+
+if [ ! -f "${TMP_FILE}" ] ; then
+	echo "\"${TMP_FILE}\" doesn't exist for some reason.  Bailing..."
+	exit 1
+fi
+
+# Alien URL
+# curl -sL http://www.smoktech.com/support/upgrade/toolsandfirmware | xmllint --nowarning --format --html - 2>/dev/null | grep -i tool.*alien | sed 's/^.*="\(.*\)">.*$/\1/'
+#
+# ZIP File
+# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/'
+#
+# Name
+# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//'
+#
+# Version
+# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2
+#
+# Date:
+# date -d "$(curl -sL --head http://7xjcby.com2.z0.glb.qiniucdn.com/file/14752125965974vmwev5prxto.zip | grep Last-Modified: | cut -d ' ' -f2-)" +%s
+
+
+
+#ZIP_FILE=$(curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
+ZIP_FILE=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
+BASENAME=$(basename "${ZIP_FILE}")
+# BASENAME=$(basename "${ZIP_FILE}" | sed 's/\([0-9]\+\).*/\1/')
+BIG_STAMP=$(basename "${ZIP_FILE}" | sed 's/\([0-9]\+\).*/\1/')
+FILE_STAMP=$(date -d  @${BASENAME:0:10} +%s)
+YESTER_STAMP=$(date -d $(date -d "yesterday" +%Y-%m-%d) +%s)
+YESTER_STAMP="$(date -d 2017-01-01 +%s)"
+
+if [ ${DEBUG} ] ; then
+	echo "TMP_FILE: ${TMP_FILE}"
+	echo "ZIP_FILE: ${ZIP_FILE}"
+	echo "BASENAME: ${BASENAME}"
+	echo "BIG_STAMP: ${BIG_STAMP}"
+	echo "FILE_STAMP: ${FILE_STAMP} ($(date -d @${FILE_STAMP}))"
+	echo "YESTER_STAMP: ${YESTER_STAMP} ($(date -d @${YESTER_STAMP}))"
+fi
+
+if [ "${FILE_STAMP}" -gt "${YESTER_STAMP}" ] ; then
+
+	VERSION=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2)
+
+	echo -e "\n\nALIEN FIRMWARE UPDATE AVAILABLE\n\n"
+	echo -e "New version: ${VERSION}"
+	echo -e "File date: $(date -d @${FILE_STAMP})"
+	echo -e "Zip file: ${ZIP_FILE}\n"
+else
+
+	echo -e "\vNO ALIEN FIRMWARE UPDATE\v"
+fi
+
+if [ ${DEBUG} ] ; then
+	rm -fv ${TMP_FILE}
+else
+	rm -f ${TMP_FILE}
+fi
+
