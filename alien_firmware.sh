89c61c43f1388649d601bc95bae3fb90ca921c96
Matt <matt.palermo@gmail.com>
2017-02-07 14:08:21 -0500

    spit out page url and also limit result to only the correct firmware

--- alien_firmware.sh
+++ alien_firmware.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# DEBUG="YES"
+#DEBUG="YES"
+
+URL="http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien"
 
 TMP_FILE="/tmp/alien_firmware_$(date +%s).tmp"
 
 UA="Mozilla/5.0 (Linux; Android 6.0; XT1585 Build/MCK24.78-13.12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.89 Mobile Safari/537.36"
 
-curl -sL -A "${UA}" "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null  > ${TMP_FILE}
+curl -sL -A "${UA}" "${URL}" | xmllint --nowarning --format --html - 2>/dev/null  > ${TMP_FILE}
 
 if [ ! -f "${TMP_FILE}" ] ; then
 	echo "\"${TMP_FILE}\" doesn't exist for some reason.  Bailing..."
@@ -17,27 +19,29 @@ fi
 # curl -sL http://www.smoktech.com/support/upgrade/toolsandfirmware | xmllint --nowarning --format --html - 2>/dev/null | grep -i tool.*alien | sed 's/^.*="\(.*\)">.*$/\1/'
 #
 # ZIP File
-# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/'
+# curl -sL "${URL}" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/'
 #
 # Name
-# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//'
+# curl -sL "${URL}" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//'
 #
 # Version
-# curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2
+# curl -sL "${URL}" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2
 #
 # Date:
 # date -d "$(curl -sL --head http://7xjcby.com2.z0.glb.qiniucdn.com/file/14752125965974vmwev5prxto.zip | grep Last-Modified: | cut -d ' ' -f2-)" +%s
 
 
 
-#ZIP_FILE=$(curl -sL "http://www.smoktech.com/support/upgrade/toolsandfirmware/ispalien" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
-ZIP_FILE=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
+#ZIP_FILE=$(curl -sL "${URL}" | xmllint --nowarning --format --html - 2>/dev/null | grep -i \.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
+VERSION=$(grep "ALIEN V" ${TMP_FILE} | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | awk '{print $2}')
+ZIP_FILE=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i ${VERSION}.*\.zip | sed 's/.*href="\(.*\.zip\).*/\1/')
 BASENAME=$(basename "${ZIP_FILE}")
 # BASENAME=$(basename "${ZIP_FILE}" | sed 's/\([0-9]\+\).*/\1/')
 BIG_STAMP=$(basename "${ZIP_FILE}" | sed 's/\([0-9]\+\).*/\1/')
 FILE_STAMP=$(date -d  @${BASENAME:0:10} +%s)
 YESTER_STAMP=$(date -d $(date -d "yesterday" +%Y-%m-%d) +%s)
 #YESTER_STAMP="$(date -d 2017-01-01 +%s)"
+
 
 if [ ${DEBUG} ] ; then
 	echo "TMP_FILE: ${TMP_FILE}"
@@ -50,9 +54,10 @@ fi
 
 if [ "${FILE_STAMP}" -gt "${YESTER_STAMP}" ] ; then
 
-	VERSION=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2)
+#	VERSION=$(xmllint --nowarning --format --html ${TMP_FILE} 2>/dev/null | grep -i \.zip | sed 's/<[^>]\+>/ /g;s/ \+//;s/)//g' | cut -d '(' -f2)
 
 	echo -e "\n\nALIEN FIRMWARE UPDATE AVAILABLE\n\n"
+	echo -e "Page: ${URL}"
 	echo -e "New version: ${VERSION}"
 	echo -e "File date: $(date -d @${FILE_STAMP})"
 	echo -e "Zip file: ${ZIP_FILE}\n"
