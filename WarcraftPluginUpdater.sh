67c48a24b61d5d44efee6cd6cdf11a654f5e8e9a
Matt <matt.palermo@gmail.com>
2017-02-10 22:17:31 -0500

    update list

--- WarcraftPluginUpdater.sh
+++ WarcraftPluginUpdater.sh
@@ -6,9 +6,10 @@
 
 ######################################################
 # USE THIS STUFF
-CURSE_PLUGINS="advancedinterfaceoptions auctionator auctioneer altoholic crossrealmassist dejacharacterstats farmhud gathermate2 gathermate2_data \
-	handynotes handynotes_azerothstoptunes handynotes_draenortreasures handynotes_legionrarestreasures handynotes_lostandfound handynotes_timelesstreasures \
-	herebedragons postal scrap scrap-cleaner sliceadmiral maxcam skada tomtom world-quest-tracker"
+CURSE_PLUGINS="advancedinterfaceoptions auctionator auctioneer altoholic crossrealmassist dejacharacterstats farmhud \
+	gathermate2 gathermate2_data handynotes handynotes_azerothstoptunes handynotes_draenortreasures \
+	handynotes_legionrarestreasures handynotes_lostandfound handynotes_timelesstreasures \
+	herebedragons postal scrap scrap-cleaner skada tomtom world-quest-tracker"
 
 ADDON_DIR="/WoW_2014_11_28/AddOns"
 
