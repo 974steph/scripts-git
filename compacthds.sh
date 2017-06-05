42b29c918e27fc70f0efa22507736d5fafee9c8e
Matt Palermo <matt.palermo@ring.com>
2017-06-05 15:21:47 -0400

    fixed

--- compacthds.sh
+++ compacthds.sh
@@ -1,22 +1,43 @@
 #!/usr/bin/env bash
 
-DEBUG="YES"
+#DEBUG="YES"
 
 SAVEIFS=${IFS}
 IFS=$(echo -en "\n\b")
 
+#for HD in $(VBoxManage list hdds | egrep "^UUID|^Location" | gsed 's/: \+/=/') ; do
+#	echo HD: $HD
+#done
+#exit
+
 for HD in $(VBoxManage list hdds | egrep "^UUID|^Location" | gsed 's/: \+/=/') ; do
+
+#	echo HD: $HD
+	if [ "$(echo $HD | grep UUID)" ] ; then
+		[ ${DEBUG} ] && echo "FOUND UUID"
+		eval $HD
+		[ ${DEBUG} ] && echo -e "\\tUUID: \"$UUID\""
+	fi
+
+	if [ "$(echo $HD | grep Location)" ] ; then
+		[ ${DEBUG} ] && echo "FOUND LOCATION"
+		HD=$(echo $HD | sed 's/ /\\ /')
+		eval "$HD"
+		[ ${DEBUG} ] && echo -e "\\tLocation: \"$Location\""
+	fi
 
 	if [ "${UUID}" -a "${Location}" ] ; then
 
 		[ ${DEBUG} ] && echo "GOT BOTH"
 
-		NAME=$(basename "${Location}")
-		echo -e "\\tNAME: ${NAME}"
+		basename "${Location}"
+#		NAME=$(basename "${Location}")
+#		echo -e "\\tNAME: ${NAME}"
+#		echo "${NAME}"
 
 		echo "Old Size: $(du -h "${Location}" | awk '{print $1}')"
 
-echo		VBoxManage modifyhd --compact "${UUID}"
+		VBoxManage modifyhd --compact "${UUID}"
 
 		echo "New Size: $(du -h "${Location}" | awk '{print $1}')"
 
@@ -25,20 +46,9 @@ echo		VBoxManage modifyhd --compact "${UUID}"
 
 		echo "========="
 	else
-		if [ "$(echo $HD | grep UUID)" ] ; then
-			[ ${DEBUG} ] && echo "FOUND UUID"
-			eval $HD
-			[ ${DEBUG} ] && echo -e "\\tUUID: \"$UUID\""
-		fi
-
-		if [ "$(echo $HD | grep Location)" ] ; then
-			[ ${DEBUG} ] && echo "FOUND LOCATION"
-			HD=$(echo $HD | sed 's/ /\\ /')
-			eval "$HD"
-			[ ${DEBUG} ] && echo -e "\\tLocation: \"$Location\""
-		fi
 
 #		[ ${DEBUG} ] && echo "HD: $HD"
+		[ ${DEBUG} ] && echo "LOOP"
 	fi
 
 #	[ ${DEBUG} ] && echo "========="
