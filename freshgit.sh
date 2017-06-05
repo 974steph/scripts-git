d51ecae32055398a2306f3b0d80a2bd8f61d6e37
Matt Palermo <matt.palermo@ring.com>
2017-06-05 08:51:40 -0400

    prettiness

--- freshgit.sh
+++ freshgit.sh
@@ -1,29 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
-TANKS="$HOME/GIT $HOME/PROJ"
+TANKS="$HOME/GIT"
+NOW=$(date +%s)
+UPDATED=0
+GIT_COUNTER=1
+CHANGED=0
+INSERTS=0
+DELETES=0
 
-X=1
-G=1
-GITS=0
+R=$(tput setaf 1)
+P=$(tput setaf 5)
+LB=$(tput setaf 6)
+B=$(tput bold)
+N=$(tput sgr0)
+
 
 for DIR in ${TANKS} ; do
-	GITS=$(( $GITS + $(find ${DIR} -type d -maxdepth 1 -mindepth 1 | wc -l) ))
+	GITS=$(( ${GITS} + $(find ${DIR} -type d -maxdepth 1 -mindepth 1 | wc -l) ))
 done
 
 echo
 
-for GIT in $TANKS ; do
-	for DIR in $(find ${GIT} -type d -maxdepth 1 -mindepth 1) ; do 
+for GIT in ${TANKS} ; do
+	for DIR in $(find ${GIT} -type d -maxdepth 1 -mindepth 1) ; do
+
 		cd "${DIR}"
-		echo "$(basename ${DIR}) ($G of $GITS)"
-		git pull
-		X=$(( ${X} + 1 ))
-		if [ ${X} -le ${GITS} ] ; then
-			echo "---------"
+
+		echo "${B}$(basename ${DIR})${N} (${GIT_COUNTER} of ${GITS})"
+
+		OUTPUT=$(git pull 2>&1 | egrep -i "changed.*insertions.*deletions|up-to-date" | gsed 's/^\ \+//')
+
+		if [[ "${OUTPUT}" =~ .*changed.*insertions.*deletions.* ]] ; then
+			echo -e "${LB}${OUTPUT}${N}"
+			UPDATED=$(( ${UPDATED} + 1 ))
+			CHANGED=$(( ${CHANGED} + $(echo ${OUTPUT} | awk '{print $1}') ))
+			INSERTS=$(( ${INSERTS} + $(echo ${OUTPUT} | awk '{print $4}') ))
+			DELETES=$(( ${DELETES} + $(echo ${OUTPUT} | awk '{print $6}') ))
 		else
-			echo
+			echo -e "${R}${OUTPUT}${N}"
 		fi
 
-		G=$(( $G + 1 ))
+		GIT_COUNTER=$(( ${GIT_COUNTER} + 1 ))
+
+		[ ${GIT_COUNTER} -le ${GITS} ] && echo "---------"
 	done
 done
+
+if [ ${UPDATED} -gt 0 ] ; then
+	echo -e "\\v${B}${P}It took $(( $(date +%s) - ${NOW} )) seconds to update ${UPDATED} of ${GITS} repos.${N}"
+#	echo -e "Totals:"
+	echo -e "\\tFiles Changed: ${CHANGED}"
+	echo -e "\\tInsertions: ${INSERTS}"
+	echo -e "\\tDeletions: ${DELETES}"
+else
+	echo -e "\\v${B}${P}It took $(( $(date +%s) - ${NOW} )) seconds to check ${GITS} repos.${N}\\v"
+fi
