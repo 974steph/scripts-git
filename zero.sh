f74d6a9c964b3ca35967071c1488aa8e7c891a7c
Matt <matt.palermo@gmail.com>
2017-06-15 09:23:08 -0400

    more

--- zero.sh
+++ zero.sh
@@ -18,7 +18,8 @@ if [ -f /etc/lsb-release ] ; then
 
 	if [[ $DISTRIB_ID =~ .*Gentoo.* ]] ; then
 		source /etc/portage/make.conf
-		find "${DISTDIR}" -type f -exec rm -fv "{}" \;
+		find "${DISTDIR}" -type f -exec rm -f "{}" \;
+		find "${PORT_LOGDIR}" -mtime +14 -type f -exec rm -f "{}" \;
 		MOUNTS="${HOME} /usr/local"
 		WORKS=TRUE
 	elif [[ $DISTRIB_ID =~ .*Arch.* ]] ; then
