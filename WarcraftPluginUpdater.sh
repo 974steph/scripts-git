b28c47e0c930e1b82acea0a463081affa9d7b96a
Matt <matt.palermo@gmail.com>
2017-09-19 18:25:57 -0400

    +datastore_characters

--- WarcraftPluginUpdater.sh
+++ WarcraftPluginUpdater.sh
@@ -3,8 +3,8 @@
 
 ######################################################
 # EDIT THIS STUFF FOR YOUR SYSTEM / SETUP
-CURSE_PLUGINS="askmrrobot auctionator auctioneer altoholic bagnon datastore deadly-boss-mods \
-	dejacharacterstats farmhud gathermate2 gathermate2_data handynotes \
+CURSE_PLUGINS="askmrrobot auctionator auctioneer altoholic bagnon datastore datastore_characters 
+	deadly-boss-mods dejacharacterstats farmhud gathermate2 gathermate2_data handynotes \
 	handynotes_legionrarestreasures pawn postal scrap scrap-cleaner skada tomtom world-quest-tracker"
 
 ADDON_DIR="/WoW_2014_11_28/AddOns"
