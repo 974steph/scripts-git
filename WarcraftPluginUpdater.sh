d069beddade8c593c2816780548ee1a2e088e485
Matt <matt.palermo@gmail.com>
2017-09-19 18:29:00 -0400

    +datastore_achievements +datastore_agenda

--- WarcraftPluginUpdater.sh
+++ WarcraftPluginUpdater.sh
@@ -3,7 +3,7 @@
 
 ######################################################
 # EDIT THIS STUFF FOR YOUR SYSTEM / SETUP
-CURSE_PLUGINS="askmrrobot auctionator auctioneer altoholic bagnon datastore datastore_characters 
+CURSE_PLUGINS="askmrrobot auctionator auctioneer altoholic bagnon datastore datastore_achievements datastore_agenda datastore_characters \
 	deadly-boss-mods dejacharacterstats farmhud gathermate2 gathermate2_data handynotes \
 	handynotes_legionrarestreasures pawn postal scrap scrap-cleaner skada tomtom world-quest-tracker"
 
