2d5e4b1f244009c171658f0d40f0c178fb7189c6
Matt <matt.palermo@gmail.com>
2017-09-24 13:02:06 -0400

    many tweaks, cleaning, and improved predictability

--- redo_vid_x264.sh
+++ redo_vid_x264.sh
@@ -4,12 +4,18 @@
 
 trap ctrl_c INT
 
-AUDIO_BITRATE_TARGET=256
+###########################
+# VARS
 PRESET_V="fast"
-PRESET_A="medium"
+PRESET_A="fast"
+VBR=5
 WIDTH_MAX="1080"
 
+DIR_TEMP="TempFiles"
+DIR_FINAL="Final"
+###########################
 
+###########################
 if [ ! $# -eq 2 ] ; then
 	echo -e "\\vGive me a file and a size.\\v"
 	echo -e "$(basename $0) [FILE] [size|auto]\\v"
@@ -22,17 +28,21 @@ else
 		exit 1
 	fi
 
+	eval $(midentify "${INFILE}" | egrep "ID_VIDEO_WIDTH|ID_VIDEO_HEIGHT|ID_LENGTH")
 
 	JUSTNAME="$(echo ${INFILE} | sed 's/\.[a-zA-Z]\+$//')"
-	OUTFILE_V="${JUSTNAME}_pass2.mp4"
-	OUTFILE_A="${JUSTNAME}.aac"
-	OUTFILE_FINAL="${JUSTNAME}.mp4"
+	OUTFILE_V="${DIR_TEMP}/${JUSTNAME}_pass2.mp4"
+	OUTFILE_A="${DIR_TEMP}/${JUSTNAME}.aac"
+	OUTFILE_FINAL="${DIR_FINAL}/${JUSTNAME}.mp4"
 
 	if [ ${DEBUG} ] ; then
 
 		V=v
-		TIME=time
 
+		echo "ID_VIDEO_WIDTH: $ID_VIDEO_WIDTH"
+		echo "ID_VIDEO_HEIGHT: $ID_VIDEO_HEIGHT"
+		echo "ID_LENGTH: $ID_LENGTH"
+		echo "---------"
 		echo "INFILE: $INFILE"
 		echo "JUSTNAME: $JUSTNAME"
 		echo "OUTFILE_V: $OUTFILE_V"
@@ -42,8 +52,6 @@ else
 	fi
 fi
 
-eval $(midentify "${INFILE}" | egrep "ID_VIDEO_WIDTH|ID_VIDEO_HEIGHT|ID_LENGTH")
-
 case $2 in
 	auto)
 #		WANT_SIZE=$(du -m "${INFILE}" | awk '{print $1}')
@@ -52,30 +60,19 @@ case $2 in
 		;;
 	*)
 		WANT_SIZE=$2
+		VBR=3
 		;;
 esac
-
-
-[ -f ffmpeg2pass-0.log ] && rm -f${V} ffmpeg2pass-0.log
-[ -f ffmpeg2pass-0.log.mbtree ] && rm -f${V} ffmpeg2pass-0.log.mbtree
-
-
-
-if [ ${DEBUG} ] ; then
-	echo "ID_VIDEO_WIDTH: $ID_VIDEO_WIDTH"
-	echo "ID_VIDEO_HEIGHT: $ID_VIDEO_HEIGHT"
-	echo "ID_LENGTH: $ID_LENGTH"
-fi
+###########################
 
 
 if [ "${ID_VIDEO_WIDTH}" -gt ${WIDTH_MAX} ] ; then
-	[ ${DEBUG} ] && echo "Resizing to ${WIDTH_MAX}"
 	VIDEO_WIDTH_FINAL=${WIDTH_MAX}
+	echo -e "\\vResizing to ${VIDEO_WIDTH_FINAL}"
 else
-	[ ${DEBUG} ] && echo "Keeping current width"
 	VIDEO_WIDTH_FINAL="${ID_VIDEO_WIDTH}"
+	[ ${DEBUG} ] && echo "Keeping current width of ${VIDEO_WIDTH_FINAL}"
 fi
-[ ${DEBUG} ] && echo "VIDEO_WIDTH_FINAL: ${VIDEO_WIDTH_FINAL}"
 
 WIDTH_CHANGE=$(echo "${WIDTH_MAX} / ${ID_VIDEO_WIDTH}" | bc -l)
 [ ${DEBUG} ] && echo "WIDTH_CHANGE: ${WIDTH_CHANGE}"
@@ -83,104 +80,124 @@ WIDTH_CHANGE=$(echo "${WIDTH_MAX} / ${ID_VIDEO_WIDTH}" | bc -l)
 VIDEO_HEIGHT_FINAL=$(echo "((${ID_VIDEO_HEIGHT} * ${WIDTH_CHANGE}) / 2) * 2" | bc)
 [ ${DEBUG} ] && echo "VIDEO_HEIGHT_FINAL: ${VIDEO_HEIGHT_FINAL}"
 
-
 [ ${DEBUG} ] && echo -e "\\v---------\\v"
 
 
+###########################
+# TRAP CTRL+C
 function ctrl_c() {
 	exit 2
 }
+###########################
 
 
+###########################
+# ERROR
+function errorcode() {
+
+	case $2 in
+		0) 	echo -e "\\v$1 exited $2.  Continuing...\\v"
+			unset TRAP;;
+		*)	echo -e "\\v$1 exited $2.  Bailing...\\v"
+			exit $2
+	esac
+}
+###########################
+
+
+###########################
+# DO AUDIO
 function doaudio() {
 
 	echo -e "Writing audio file \"${OUTFILE_A}\""
 
-	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -vn -c:a libfdk_aac -vbr 5 \"${OUTFILE_A}\""
-	ffmpeg -v 3 -stats -y -i "${INFILE}" -vn -c:a libfdk_aac -vbr 5 -threads 0  "${OUTFILE_A}"
+	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -vn -c:a libfdk_aac -vbr ${VBR} \"${OUTFILE_A}\""
+	ffmpeg -v 3 -stats -y -i "${INFILE}" -vn -c:a libfdk_aac -vbr ${VBR} -threads 0 "${OUTFILE_A}"
 	TRAP=$?
 
-	[ ${DEBUG} ] && echo "DOAUDIO: ${TRAP}"
-
-	unset TRAP
+	[ ${DEBUG} ] && errorcode doaudio $TRAP
 }
+###########################
 
+
+###########################
+# DO VIDEO 1
 function dovideo1() {
 
-	echo -e "Encode video - pass 1"
+	echo -e "\\vGetting pass 1 stats from \"${INFILE}\""
 
-	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -c:v libx264 -b:v ${BITRATE_FINAL}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 1 -threads 0 -f mp4 /dev/null"
-	ffmpeg -v 3 -stats -y -i "${INFILE}" -c:v libx264 -b:v ${BITRATE_FINAL}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 1 -threads 0 -f mp4 /dev/null
+	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -c:v libx264 -b:v ${BITRATE_VIDEO}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 1 -passlogfile \"${DIR_TEMP}/ffmpeg2pass\" -threads 0 -f mp4 /dev/null"
+	ffmpeg -v 3 -stats -y -i "${INFILE}" -c:v libx264 -b:v ${BITRATE_VIDEO}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 1 -passlogfile "${DIR_TEMP}/ffmpeg2pass" -threads 0 -f mp4 /dev/null
 	TRAP=$?
 
-	[ ${DEBUG} ] && echo "DOVIDEO1: ${TRAP}"
-
-	unset TRAP
+	[ ${DEBUG} ] && errorcode dovideo1 $TRAP
 }
+###########################
 
+
+###########################
+# DO VIDEO 2
 function dovideo2() {
 
-	echo -e "Writing video file \"${OUTFILE_V}\""
+	echo -e "\\vWriting video file \"${OUTFILE_V}\""
 
-	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -c:v libx264 -b:v ${BITRATE_FINAL}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 2 -threads 0 -f mp4 \"${OUTFILE_V}\""
-	ffmpeg -v 3 -stats -y -i "${INFILE}" -c:v libx264 -b:v ${BITRATE_FINAL}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 2 -threads 0 -f mp4 "${OUTFILE_V}"
+	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${INFILE}\" -c:v libx264 -b:v ${BITRATE_VIDEO}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 2 -passlogfile \"${DIR_TEMP}/ffmpeg2pass\" -threads 0 -f mp4 \"${OUTFILE_V}\""
+	ffmpeg -v 3 -stats -y -i "${INFILE}" -c:v libx264 -b:v ${BITRATE_VIDEO}k -preset:v ${PRESET_V} -s ${VIDEO_WIDTH_FINAL}:${VIDEO_HEIGHT_FINAL} -an -pass 2 -passlogfile "${DIR_TEMP}/ffmpeg2pass" -threads 0 -f mp4 "${OUTFILE_V}"
 	TRAP=$?
 
-	[ ${DEBUG} ] && echo "DOVIDEO1: ${TRAP}"
-
-	unset TRAP
+	[ ${DEBUG} ] && errorcode dovideo2 $TRAP
 }
+###########################
 
+
+###########################
+# COMBINE PARTS
 function combine() {
 
-	echo -e "Writing final file \"${OUTFILE_FINAL}\""
+	echo -e "\\vWriting final file \"${OUTFILE_FINAL}\""
+
+	[ ! -d "${DIR_FINAL}" ] && mkdir -p "${DIR_FINAL}"
 
 	[ ${DEBUG} ] && echo "ffmpeg -v 3 -stats -y -i \"${OUTFILE_V}\" -i \"${OUTFILE_A}\" -c:v copy -map 0:v:0 -map 1:a:0 -c:a copy \"${OUTFILE_FINAL}\""
 	ffmpeg -v 3 -stats -y -i "${OUTFILE_V}" -i "${OUTFILE_A}" -c:v copy -map 0:v:0 -map 1:a:0 -c:a copy "${OUTFILE_FINAL}"
 	TRAP=$?
 
-	[ ${DEBUG} ] && echo "COMBINE: ${TRAP}"
-
-	unset TRAP
+	[ ${DEBUG} ] && errorcode combine $TRAP
 }
+###########################
 
 
+[ ! -d "${DIR_TEMP}" ] && mkdir -p "${DIR_TEMP}"
+[ -f "${DIR_TEMP}/ffmpeg2pass-0.log" ] && rm -f${V} "${DIR_TEMP}/ffmpeg2pass-0.log"
+[ -f "${DIR_TEMP}/ffmpeg2pass-0.log.mbtree" ] && rm -f${V} "${DIR_TEMP}/ffmpeg2pass-0.log.mbtree"
 
 echo
 
-${TIME} doaudio
+doaudio
 
-echo -e "\\v---------\\v"
+[ ${DEBUG} ] && echo -e "\\v---------\\v"
 
-#AUDIO_SIZE=$(du -k "${OUTFILE_A}" | awk '{print $1}')
 AUDIO_SIZE=$(du -m "${OUTFILE_A}" | awk '{print $1}')
 [ ${DEBUG} ] && echo "AUDIO_SIZE: $AUDIO_SIZE"
 
-#BITRATE_VIDEO=$(echo "((${WANT_SIZE} * 8192) - ${AUDIO_SIZE}) / ${ID_LENGTH}" | bc)
-BITRATE_VIDEO=$(echo "((${WANT_SIZE} * 8192) - (${AUDIO_SIZE} * 8192)) / ${ID_LENGTH}" | bc)
-#BITRATE_VIDEO=$(echo "((${WANT_SIZE} * 1024) - ${AUDIO_SIZE}) / ${ID_LENGTH}" | bc)
+#BITRATE_VIDEO=$(echo "((${WANT_SIZE} * 8192) - (${AUDIO_SIZE} * 8192)) / ${ID_LENGTH}" | bc)
+BITRATE_VIDEO=$(echo "((${WANT_SIZE} - ${AUDIO_SIZE}) * 8192) / ${ID_LENGTH}" | bc)
 [ ${DEBUG} ] && echo "BITRATE_VIDEO: $BITRATE_VIDEO"
 
-#BITRATE_FINAL=$(( ${BITRATE_VIDEO} - ${AUDIO_BITRATE_TARGET}))
-BITRATE_FINAL=${BITRATE_VIDEO}
-[ ${DEBUG} ] && echo "BITRATE_FINAL: ${BITRATE_FINAL}"
+dovideo1
 
-${TIME} dovideo1
+[ ${DEBUG} ] && echo -e "\\v---------\\v"
 
-echo -e "\\v---------\\v"
+dovideo2
 
-${TIME} dovideo2
+[ ${DEBUG} ] && echo -e "\\v---------\\v"
 
-echo -e "\\v---------\\v"
+combine
 
-${TIME} combine
+[ ${DEBUG} ] && echo -e "\\v---------\\v"
 
-echo -e "\\v---------\\v"
 
 echo "Original: $(du -m "${INFILE}" | sed 's/\t/M - /')"
-
 echo "Temp Video: $(du -m "${OUTFILE_V}" | sed 's/\t/M - /')"
-
 echo "Temp Audio: $(du -m "${OUTFILE_A}" | sed 's/\t/M - /')"
-
 echo "Final File: $(du -m "${OUTFILE_FINAL}" | sed 's/\t/M - /')"
