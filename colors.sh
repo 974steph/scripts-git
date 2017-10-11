5d0b2074479f7a8a64389c00244d9f616320e284
Matt <matt.palermo@gmail.com>
2017-10-11 16:07:20 -0400

    makes more sense

--- colors.sh
+++ colors.sh
@@ -5,8 +5,10 @@ SO=$(tput smso)
 N=$(tput sgr0)
 BOLD=$(tput bold)
 
-for F in $(seq 1 7) ; do
-	for B in $(seq 1 7) ; do
-		echo "${BOLD}${SO} $(tput setaf $F) $(tput setab $B)$F $B IJDHKDHKDHKHKJHDGKH ${N}"
+for B in $(seq 1 7) ; do
+	for F in $(seq 1 7) ; do
+		echo -e "${BOLD}\\t$(tput setab $B)$(tput setaf $F) Background: $B || Foreground: $F ${N}"
 	done
+
+	echo
 done
