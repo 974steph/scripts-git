bbd4538e2d078d62b239df53b9380f7b8c7de160
Matt <matt.palermo@gmail.com>
2017-10-25 13:02:38 -0400

    ugly but finds the best one

--- SternTPB.sh
+++ SternTPB.sh
@@ -19,6 +19,7 @@ case $1 in
 		;;
 esac
 
+TMPFILE="/tmp/$(basename $0).tmp"
 MAG_DATE_WORD=$(date -d ${DATE} +%b.%-d.%Y)
 MAG_DATE_NUM=$(date -d ${DATE} +%-m.%d.%Y)
 MAG_DATE_LWORD=$(date -d ${DATE} +%Y\.%B\.%d)
@@ -62,6 +63,8 @@ function findbest() {
 	for POSSIBLE in ${POSSIBLES} ; do
 
 		BITRATE=$(echo "${POSSIBLE}" | sed 's/.*+\([0-9]\+\)[kK]+.*/\1/')
+		MAGID=$(echo "${POSSIBLE}" | sed 's/^.*btih:\(.*\)&amp;dn=.*/\1/')
+		[ ${DEBUG} ] && echo "MAGID: $MAGID"
 
 		if [ ! "$(echo ${BITRATE} | grep [a-zA-Z])" ] ; then
 
@@ -76,9 +79,11 @@ function findbest() {
 				[ ${DEBUG} ] && echo "ELSE BESTBIT: $BESTBIT || BITRATE: $BITRATE"
 			fi
 		else
-			[ ${DEBUG} ] && echo "NOT FOUND: ${BITRATE}"
+			[ ${DEBUG} ] && echo "NO BITRATE FOUND: ${BITRATE}"
 			FALLBACK="${POSSIBLE}"
 		fi
+
+		[ ${DEBUG} ] && echo "---------"
 	done
 
 	IFS=${SAVEIFS}
@@ -98,18 +103,130 @@ function findbest() {
 	echo "Using: ${MAG_PRETTY}"
 }
 
+function bestbit() {
+	[ ! ${BESTBIT} ] && BESTBIT=${MAG_BITRATE}
+
+	if [ ${MAG_BITRATE} -ge ${BESTBIT} ] ; then
+		[ ${DEBUG} ] && echo "Updating BESTBIT to: ${MAG_BITRATE}"
+		BESTBIT=${MAG_BITRATE}
+		BESTBITUID=${MAG_UID}
+#		echo BESTBITUID: ${MAG_UID}
+		BESTBITMAG=${POSSIBLE}
+		[ ${DEBUG} ] && echo "IF BESTBIT: $BESTBIT || MAG_BITRATE: $MAG_BITRATE"
+	else
+		[ ${DEBUG} ] && echo "ELSE BESTBIT: $BESTBIT || MAG_BITRATE: $MAG_BITRATE"
+	fi
+}
+
+function bestsize() {
+	[ ! ${BESTSIZE} ] && BESTSIZE=${MAG_SIZE}
+
+	if [ ${MAG_SIZE} -ge ${BESTSIZE} ] ; then
+		[ ${DEBUG} ] && echo "Updating BESTSIZE to: ${MAG_SIZE}"
+		BESTSIZE=${MAG_SIZE}
+		BESTSIZEUID=${MAG_UID}
+		BESTSIZEMAG=${POSSIBLE}
+		[ ${DEBUG} ] && echo "IF BESTBIT: $BESTSIZE || MAG_SIZE: $MAG_SIZE"
+	else
+		[ ${DEBUG} ] && echo "ELSE BESTSIZE: $BESTSIZE || MAG_SIZE: $MAG_SIZE"
+	fi
+}
+
+function magstats() {
+
+	SAVE_IFS=$IFS
+	IFS=$'\n'
+
+	for POSSIBLE in ${POSSIBLES} ; do
+
+		MAG_UID=$(echo "${POSSIBLE}" | sed 's/^.*btih:\(.*\)&amp;dn=.*/\1/')
+		MAG_BITRATE=$(echo "${POSSIBLE}" | sed 's/.*+\([0-9]\+\)[kK]+.*/\1/')
+		[ "$(echo ${MAG_BITRATE} | grep [a-zA-Z])" ] && MAG_BITRATE=0
+#		MAG_SIZE=$(curl -sL "${SEARCH_URL}" | grep -A2 -i "${MAG_UID}" | grep Size | sed 's/^.*Size \(.*\)\..*&nbsp\;MiB.*/\1/')
+		MAG_SIZE=$(grep -A2 -i "${MAG_UID}" ${TMPFILE} | grep Size | sed 's/^.*Size \(.*\)\..*&nbsp\;MiB.*/\1/')
+
+		if [ ${DEBUG} ] ; then
+			echo "MAG_UID: \"$MAG_UID\""
+			echo "MAG_BITRATE: \"$MAG_BITRATE\""
+			echo "MAG_SIZE: \"$MAG_SIZE\""
+#			echo "---------"
+		fi
+
+		bestbit
+
+		bestsize
+
+		if [ ${DEBUG} ] ; then
+			echo "BESTBITUID: $BESTBITUID"
+			echo "BESTBITMAG: $BESTBITMAG"
+			echo "BESTBIT: $BESTBIT"
+			echo "~~~~~~~~~"
+			echo "BESTSIZEUID: $BESTSIZEUID"
+			echo "BESTSIZEMAG: $BESTSIZEMAG"
+			echo "BESTSIZE: $BESTSIZE"
+		fi
+
+	done
+
+#		if [ ! ${BESTMAG} ] ; then
+			if [[ "${BESTSIZEUID}" == "${BESTBITUID}" ]] ; then
+
+				echo "Size ${BESTSIZE} and Bitrate ${BESTBIT} wins"
+#				MAG_RAW_UID=$(grep -i magnet:.*${BESTBITMAG} ${TMPFILE} | sed 's/^.*\(magnet:.*6969\)" title=".*/\1/')
+				MAG_RAW_UID=${BESTBITUID}
+				MAG_RAW=${BESTBITMAG}
+#				BESTMAG=yes
+
+			elif [ "${BESTBIT}" -ge 128 ] ; then
+				echo "Bitrate of ${BESTBIT} wins"
+#				MAG_RAW_UID=$(grep -i magnet:.*${BESTBITMAG} ${TMPFILE} | sed 's/^.*\(magnet:.*6969\)" title=".*/\1/')
+				MAG_RAW_UID=${BESTBITUID}
+				MAG_RAW=${BESTBITMAG}
+#				BESTMAG=yes
+			else
+				echo "Falling back to size of ${BESTSIZE}"
+#				MAG_RAW_UID=$(grep -i magnet:.*${BESTSIZEMAG} ${TMPFILE} | sed 's/^.*\(magnet:.*6969\)" title=".*/\1/')
+				MAG_RAW_UID=${BESTSIZEUID}
+				MAG_RAW=${BESTSIZEMAG}
+#				BESTMAG=yes
+			fi
+
+			MAG_PRETTY=$(echo "${MAG_RAW}" | sed 's/&amp;tr=.*//g;s/^.*dn=\(.*\)/\1/;s/+/ /g')
+#			MAG_RAW_UID=${MAG_RAW_UID}
+			echo
+			echo "BEST MAG_RAW_UID: ${MAG_RAW_UID}"
+			echo "BEST MAG_RAW: ${MAG_RAW}"
+
+			[ ${DEBUG} ] && echo "========="
+#		fi
+}
+
+
+
 
 #RESULTS=$(curl -sL -A "${UA}" "${SEARCH_URL}" | grep magnet:)
-RESULTS=$(curl -sL "${SEARCH_URL}" | grep magnet:)
+#RESULTS=$(curl -sL "${SEARCH_URL}" | grep magnet:)
+
+[ ! -f ${TMPFILE} ] && curl -sL "${SEARCH_URL}" > ${TMPFILE}
 
 #POSSIBLES=$(echo ${RESULTS} | tidy - 2>/dev/null | egrep -i "Howard.*Stern.*${MAG_DATE_LEADING}|Howard.*Stern.*${MAG_DATE_LWORD}|Howard.*Stern.*${MAG_DATE_NUM}|Howard.*Stern.*${MAG_DATE_WORD}")
-POSSIBLES=$(echo ${RESULTS} | tidy - 2>/dev/null | egrep -i "${MAG_DATE_LEADING}|${MAG_DATE_LWORD}|${MAG_DATE_NUM}|${MAG_DATE_WORD}" | sed 's/"//g')
+#POSSIBLES=$(echo ${RESULTS} | tidy - 2>/dev/null | egrep -i "${MAG_DATE_LEADING}|${MAG_DATE_LWORD}|${MAG_DATE_NUM}|${MAG_DATE_WORD}" | sed 's/"//g')
+POSSIBLES=$(grep magnet: ${TMPFILE} | tidy - 2>/dev/null | egrep -i "${MAG_DATE_LEADING}|${MAG_DATE_LWORD}|${MAG_DATE_NUM}|${MAG_DATE_WORD}" | sed 's/"//g')
+
+#echo $POSSIBLES
+#exit
+
+#magstats
+
+
+#exit
 
 if [ "${POSSIBLES}" ] ; then
 
 	echo -e "\\vShow for ${DATE} available!"
 
-	findbest
+#	findbest
+	magstats
 
 	[ ${CHECK} ] && exit 0
 
